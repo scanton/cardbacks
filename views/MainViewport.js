84c39060d9e9e6d7ebb1b9c204d6ef1f97e5768a
scanton <scanton@users.noreply.github.com>
2019-11-05 22:29:00 -0600

    playing

--- views/MainViewport.js
+++ views/MainViewport.js
@@ -2,8 +2,12 @@
 	var componentName = 'main-viewport';
 	var s = `
 		<div class="` + componentName + ` text-center" :style="portStyle">
-			<div class="stage" :style="stageStyle">
-				<div class="background-design" v-html="design"></div>
+			<div class="background-container">
+				<div class="background-layer-1 background-layer"></div>
+				<div class="background-layer-2 background-layer"></div>
+				<div class="stage" :style="stageStyle">
+					<div class="background-design" v-html="design"></div>
+				</div>
 			</div>
 		</div>
 	`;
